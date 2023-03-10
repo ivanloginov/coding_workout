05820b6889e030e6d7a14dbee95b8a379f902309
jaymanvirk <jay.manvirk@gmail.com>
2023-03-10 14:08:35 +0100

    updated min_cost_stairs to TO=O(1)

--- solutions/min_cost_climbing_stairs.py
+++ solutions/min_cost_climbing_stairs.py
@@ -1,14 +1,12 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        cost.insert(0,0)
-        l = len(cost)
-        memo = [-1] * l
-        def dp(i):
-            if i > l - 1:
-                return 0
-            if memo[i]<0:
-                memo[i] = cost[i] + min(dp(i+1), dp(i+2))
-            return memo[i]
-
-
-        return dp(0)
+        cost.append(0)
+        n = len(cost)
+        step2 = cost[0]
+        step1 = cost[1]
+        for i in range(2,n):
+            result = cost[i] + min(step1,step2)
+            step2 = step1
+            step1 = result
+            
+        return result
