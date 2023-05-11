5c20aa609c71eb7f663bc063181df9dcb5f3b0f1
ianmanvirk <ian.manvirk@gmail.com>
2023-05-11 17:44:57 +0200

    initial commit

---
+++ solutions/best_time_to_buy_and_sell_stock_with_transaction_fee.py
@@ -0,0 +1,38 @@
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        n = len(prices)
+        # BOTTOM-UP
+        dp = {}
+        dp[0] = {0:0,1:0}
+        dp[1] = dp[0]
+        for i in range(n-1,-1,-1):
+            for j in range(2):
+                if j>0:
+                    p = dp[1][0] - prices[i]
+                else:
+                    p = dp[1][1] + prices[i] - fee
+                dp[0][j] = max(dp[1][j],p)
+            dp[1] = dp[0]
+                
+        return dp[0][1]
+
+        # BOTTOM-UP
+        # memo = {}
+        # for i in range(n):
+        #     memo[i] = {}
+        # def dp(i, hold_stock):
+        #     if i > n-1:
+        #         return 0
+        #     if hold_stock not in memo[i]:
+        #         k = 0
+        #         p = dp(i+1, hold_stock)
+        #         if hold_stock>0:
+        #             k = 1
+        #         else:
+        #             k = 0
+        #         p = max(p, (2*k-1)*prices[i] - k*fee + dp(i+1,1-k))
+        #         memo[i][hold_stock] = p
+
+        #     return memo[i][hold_stock]
+
+        # return dp(0,0)
